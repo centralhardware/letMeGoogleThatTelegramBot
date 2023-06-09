d26bdb85e14381025309fdc3b64bd3d40d5cb177
Alexey Fedechkin <alex@centralhardware.me>
2023-06-09 20:09:32 +0200

    add more sites and call clickhouse insert async

--- src/main/java/me/centralhardware/telegram/user/bot/Bot.java
+++ src/main/java/me/centralhardware/telegram/user/bot/Bot.java
@@ -3,19 +3,15 @@ package me.centralhardware.telegram.user.bot;
 import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.AnswerInlineQuery;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.InlineQuery;
-import org.telegram.telegrambots.meta.api.objects.inlinequery.inputmessagecontent.InputMessageContent;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.inputmessagecontent.InputTextMessageContent;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResultArticle;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -25,33 +21,68 @@ public class Bot extends TelegramLongPollingBot {
        super(System.getenv("BOT_TOKEN"));
    }
 
-    @SneakyThrows(TelegramApiException.class)
     @Override
     public void onUpdateReceived(Update update){
         if (!update.hasInlineQuery()) return;
 
-        clickhouse.insert(update);
+        CompletableFuture.runAsync(() -> clickhouse.insert(update));
 
         InlineQuery inlineQuery = update.getInlineQuery();
 
         String query = inlineQuery.getQuery();
 
-        InlineQueryResultArticle article = InlineQueryResultArticle.builder()
-                .id("1")
-                .title("let me google that for you")
-                .inputMessageContent(InputTextMessageContent.builder()
-                        .messageText(String.format("<a href=\"https://letmegooglethat.com/?q=%s\">let me google that for you</a>", query))
-                        .parseMode("HTML")
-                        .build())
-                .build();
+        InlineQueryResultArticle letMeGoogleThatForYou = getArticle("letmegooglethat.com",
+                getHtmlLink(getLetMeGoogleThatUrl(query), query),
+                "1");
+        InlineQueryResultArticle lmgfty = getArticle("lmgtfy.app",
+                getHtmlLink(getLmgtfyUrl(query), query),
+                "2");
+        InlineQueryResultArticle google = getArticle("google.com",
+                getHtmlLink(getGoogleUrl(query), query),
+                "3");
 
+        send(inlineQuery, letMeGoogleThatForYou, lmgfty, google);
+    }
+
+    @SneakyThrows
+    private void send(InlineQuery inlineQuery, InlineQueryResultArticle...article){
         AnswerInlineQuery answer = AnswerInlineQuery.builder()
-                .result(article)
+                .results(List.of(article))
                 .inlineQueryId(inlineQuery.getId())
                 .build();
-
         execute(answer);
+    }
 
+    private InlineQueryResultArticle getArticle(String title, String content, String id){
+        return InlineQueryResultArticle.builder()
+                .id(id)
+                .title(title)
+                .inputMessageContent(InputTextMessageContent.builder()
+                        .messageText(content)
+                        .parseMode("HTML")
+                        .disableWebPagePreview(true)
+                        .build())
+                .build();
+    }
+
+    private String getHtmlLink(String link, String title){
+       return String.format("<a href=\"%s\">%s</a>", link, title);
+    }
+
+    private static String urlEncode(String url){
+       return URLEncoder.encode(url, StandardCharsets.UTF_8);
+    }
+
+    private static String getLetMeGoogleThatUrl(String query){
+       return String.format("https://letmegooglethat.com/?q=%s", urlEncode(query));
+    }
+
+    private static String getLmgtfyUrl(String query){
+       return String.format("https://lmgtfy.app/?q=%s", urlEncode(query));
+    }
+
+    private static String getGoogleUrl(String query){
+        return String.format("https://www.google.com/search?q=%s", urlEncode(query));
     }
 
     @Override
