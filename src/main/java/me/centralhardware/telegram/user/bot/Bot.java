1a682d27b0d7f87a9b03f517fcc81102f90cf846
Alexey Fedechkin <alex@centralhardware.me>
2023-06-12 20:41:05 +0200

    fix use pastebin

--- src/main/java/me/centralhardware/telegram/user/bot/Bot.java
+++ src/main/java/me/centralhardware/telegram/user/bot/Bot.java
@@ -58,8 +58,8 @@ public class Bot extends TelegramLongPollingBot {
         if (StringUtils.isBlank(query)){
             var shrugs = getArticle("shrugs",  "¯\\_(ツ)_/¯", "1", "");
             var usePastebin = getArticle("use pastebin",
-                    "Please use pastebin.com, gist.github.com for share code or other long read text material",
                     "2",
+                    "Please use pastebin.com, gist.github.com for share code or other long read text material",
                     "");
             send(inlineQuery, List.of(shrugs, usePastebin));
         }
