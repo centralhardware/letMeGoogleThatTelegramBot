a1201694c424e57c9505f2a25f210a5a73e91e7e
alex <alex@centralhardware.me>
2025-11-25 00:59:37 +0800

    bump ktgbotapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val ktgbotapiVersion = "29.0.0"
+val ktgbotapiVersion = "30.0.2"
 
 dependencies {
     implementation("dev.inmo:tgbotapi:$ktgbotapiVersion")
